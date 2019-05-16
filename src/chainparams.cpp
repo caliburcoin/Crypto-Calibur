8c4548d01b6ac375dbf727e7c7dba022edf3ec4b
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-05-16 07:56:08 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -249,7 +249,7 @@ public:
         nMintRequiredConfirmations = 20; //the maximum amount of confirmations until accumulated in 19
         nRequiredAccumulation = 1;
         nDefaultSecurityLevel = 100; //full security level for accumulators
-        nZerocoinHeaderVersion = 4; //Block headers must be this version once zerocoin is active
+        nZerocoinHeaderVersion = INT_MAX; //Block headers must be this version once zerocoin is active
         nZerocoinRequiredStakeDepth = 200; //The required confirmations for a zcbr to be stakable
 
         nBudget_Fee_Confirmations = 6; // Number of confirmations for the finalization fee
