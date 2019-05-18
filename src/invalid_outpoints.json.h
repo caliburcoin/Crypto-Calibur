594a526573eeec583d354fee9d937d5b32be4f5a
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-05-18 07:14:43 -0500

    Create invalid_outpoints.json.h

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,11 @@
+#ifndef calibur_INVALID_OUTPOINTS_JSON_H
+#define calibur_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif 
