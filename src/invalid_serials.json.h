eb888bb43f751b409e2ab49d230d133c5fd02f67
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-05-18 07:15:17 -0500

    Create invalid_serials.json.h

---
+++ src/invalid_serials.json.h
@@ -0,0 +1,12 @@
+#ifndef calibur_INVALID_SERIALS_JSON_H
+#define calibur_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str ="[]";
+    return str;
+}
+
+#endif
