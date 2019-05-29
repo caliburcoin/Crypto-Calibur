4707aefa18b6dd43e6076278617306f7063b8c89
James Stewart <Liquid369@gmail.com>
2019-05-28 20:55:25 -0500

    Copyright adjustment

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2016-2017 The CALIBUR Developers 
+// Copyright (c) 2016-2017 The PIVX Developers 
  //Copyright (c) 2019 The Calibur developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2014 The Bitcoin developers
-// Copyright (c) 2017 The CALIBUR Developers 
+// Copyright (c) 2017 The PIVX Developers 
  //Copyright (c) 2019 The Calibur developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
